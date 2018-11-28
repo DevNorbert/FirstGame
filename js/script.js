974a85e83d57be567b4d32eed2402399cdded7e7
Norbert Janczak <norbert.janczak03@gmail.com>
2018-11-28 21:37:43 +0100

    Number of rounds won

--- js/script.js
+++ js/script.js
@@ -2,7 +2,10 @@
 
 // Main Variables
 var output = document.getElementById('output');
-
+var result = document.getElementById('result');
+var winner;
+var winPlayer = 0;
+var winComputer = 0;
 // Buttons
 var buttonPaper = document.getElementById('paper');
 var buttonStone = document.getElementById('stone');
@@ -31,18 +34,22 @@ function randomNumber() {
 function resultGame(playerMove, random) {
     var playerMove = playerMove;
     var random = random;
+
     // computerMove = paper
     if (random === 1) {
         var computerMove = 'paper';
         // playerMove = stone
         if (playerMove === 'stone') {
             output.innerHTML = 'YOU LOST: you played ' + playerMove + ', computer played ' + computerMove;
+            winner = 'Computer';
             // playerMove = shears
         } else if (playerMove === 'shears') {
             output.innerHTML = 'YOU WON: you played ' + playerMove + ', computer played ' + computerMove;
+            winner = 'Player';
             // playerMove = paper
         } else {
             output.innerHTML = 'REMIS: you played ' + playerMove + ', computer played ' + computerMove;
+            winner = 'Remis';
         }
         // computerMove = stone
     } else if (random === 2) {
@@ -50,31 +57,52 @@ function resultGame(playerMove, random) {
         // playerMove = shears
         if (playerMove === 'shears') {
             output.innerHTML = 'YOU LOST: you played ' + playerMove + ', computer played ' + computerMove;
+            winner = 'Computer';
             // playerMove = paper
         } else if (playerMove === 'paper') {
             output.innerHTML = 'YOU WON: you played ' + playerMove + ', computer played ' + computerMove;
+            winner = 'Player';
             // playerMove = stone 
         } else {
             output.innerHTML = 'REMIS: you played ' + playerMove + ', computer played ' + computerMove;
+            winner = 'Remis';
         }
     } else {
         var computerMove = 'shears';
         // playerMove = paper
         if (playerMove === 'paper') {
             output.innerHTML = 'YOU LOST: you played ' + playerMove + ', computer played ' + computerMove;
+            winner = 'Computer';
             // playerMove = stone
         } else if (playerMove === 'stone') {
             output.innerHTML = 'YOU WON: you played ' + playerMove + ', computer played ' + computerMove;
+            winner = 'Player';
             // playerMove = shears
         } else {
             output.innerHTML = 'REMIS: you played ' + playerMove + ', computer played ' + computerMove;
+            winner = 'Remis';
         }
     }
 }
+//Function WonRounds
+function wonRounds() {
+    // Player Win
+    if (winner === 'Player') {
+        result.innerHTML = 'Player: ' + ++winPlayer + ' - Computer: ' + winComputer;
+        // Computer Win
+    } else if (winner === 'Computer') {
+        result.innerHTML = 'Player: ' + winPlayer + ' - Computer: ' + ++winComputer;
+        // Remis
+    } else {
+        result.innerHTML = 'Player: ' + winPlayer + ' - Computer ' + winComputer;
+    }
+}
+
 
 // Function PlayerMove 
 function playerMove(move) {
     var random = randomNumber();
     var playerMove = move;
     resultGame(playerMove, random);
+    wonRounds();
 };
